d364767cf7c3aded66497c45836690de6dee1472
tomboy5566 <105775195+tomboy5566@users.noreply.github.com>
2022-07-27 16:30:40 +0800

    Update apkComplete

--- Unity_AR_20220727/Assets/Script/CardHandler.cs
+++ Unity_AR_20220727/Assets/Script/CardHandler.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
 
 namespace KuanLun
 {
@@ -8,21 +10,44 @@ namespace KuanLun
     {
         [SerializeField, Header("KID圖片")]
         private DefaultObserverEventHandler observerKID;
+        [SerializeField, Header("動畫控制器")]
+        private Animator ani;
+        [SerializeField, Header("攻擊按鈕")]
+        private Button attackButton;
+        [SerializeField, Header("虛擬按鍵走路")]
+        private VirtualButtonBehaviour vbbRun;
 
+        private string parVictory = "勝利觸發";
+        private AudioSource audBGM;
 
         private void Awake()
         {
             observerKID.OnTargetFound.AddListener(CardFound);
             observerKID.OnTargetLost.AddListener(CardLost);
+            attackButton.onClick.AddListener(Attack);
+            vbbRun.RegisterOnButtonPressed(OnRunPressed);
+            audBGM = GameObject.Find("BGM").GetComponent<AudioSource>();
+        }
+
+        private void OnRunPressed(VirtualButtonBehaviour obj)
+        {
+            print("跑起來啊!在混啊!");
         }
 
         private void CardFound()
         {
             print("找到卡片");
+            ani.SetTrigger(parVictory);
+            audBGM.Play();
         }
         private void CardLost()
         {
             print("無法偵測卡片");
+            audBGM.Stop();
+        }
+        private void Attack()
+        {
+            print("一定把你鼻樑打斷");
         }
     }
 }
